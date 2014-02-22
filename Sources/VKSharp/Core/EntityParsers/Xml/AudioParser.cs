42e215e57a7860d1ed33b01beffd4f76003a77f9
kasthack <kasthack@epicm.org>
2014-02-22 04:26:40 +0400

    cnt

--- Sources/VKSharp/Core/EntityParsers/Xml/AudioParser.cs
+++ Sources/VKSharp/Core/EntityParsers/Xml/AudioParser.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Xml;
+using System.Xml.Linq;
 using VKSharp.Core.Entities;
 using VKSharp.Core.Enums;
 
 namespace VKSharp.Core.EntityParsers.Xml {
     public class AudioParser : DefaultParser<Audio> {
-        public override bool UpdateFromFragment( XmlNode node, Audio entity ) {
+        public override bool UpdateFromFragment( XElement node, Audio entity ) {
             Action<Audio, string> parser;
-            var nodeName = node.Name;
+            var nodeName = node.Name.ToString();
             if ( GeneratedParsers.TryGetValue( nodeName, out parser ) ) {
-                parser( entity, node.InnerText );
+                parser( entity, node.Value );
                 return true;
             }
-            switch ( node.Name ) {
+            switch ( nodeName ) {
                 case "genre_id":
-                    entity.Genre = (AudioGenre) int.Parse( node.InnerText );
+                    entity.Genre = (AudioGenre) int.Parse( node.Value );
                     break;
                 default:
                     return false;
