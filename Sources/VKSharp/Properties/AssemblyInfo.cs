d75b06ba7cb8f38f3192282e1d8f96e85e02aea7
kasthack <kasthack@epicm.org>
2015-05-05 14:49:25 +0300

    version bump

--- Sources/VKSharp/Properties/AssemblyInfo.cs
+++ Sources/VKSharp/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.13.*")]
+[assembly: AssemblyVersion("0.5.14.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
