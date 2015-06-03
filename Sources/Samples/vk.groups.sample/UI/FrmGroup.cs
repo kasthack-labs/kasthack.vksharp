68614158d32e1db19c511488516ea1a718070b87
STAM <stam@epicm.org>
2015-06-03 11:25:50 +0300

    bump
    i am lazy :(

---
+++ Sources/Samples/vk.groups.sample/UI/FrmGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace vk.groups.sample.UI
+{
+    public partial class FrmGroup : Form
+    {
+        public FrmGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGroup_Load(object sender, EventArgs e)
+        {
+            cbxGroupsView.SelectedIndex = 0;
+        }
+
+        private void cbxGroupsView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbxGroupsView.SelectedIndex)
+            {
+                case 0:
+                    lvGroups.View = View.LargeIcon;
+                    break;
+                case 1:
+                    lvGroups.View = View.List;
+                    break;
+                case 2:
+                    lvGroups.View = View.SmallIcon;
+                    break;
+                case 3:
+                    lvGroups.View = View.Tile;
+                    break;
+            }
+        }
+    }
+}
