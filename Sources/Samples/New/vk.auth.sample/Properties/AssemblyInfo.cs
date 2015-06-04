3f64f5bb3425fdba4aa2f70b3f5c591f67ad0d77
STAM <stam@epicm.org>
2015-06-04 12:16:15 +0300

    restructurisation

---
+++ Sources/Samples/New/vk.auth.sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("vk.auth.sample")]
+[assembly: AssemblyDescription("Vk.Auth.Api sample app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("kasthack")]
+[assembly: AssemblyProduct("vk.auth.sample")]
+[assembly: AssemblyCopyright("kasthack")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f3e5e7f5-75f0-4354-bc55-1d7bc18c72b4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sources/Samples/New/vk.auth.sample/UI/FrmMain.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace vk.groups.sample.UI
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void lnkGH_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/kasthack/VKSharp");
+        }
+
+        private void checkAgree_CheckedChanged(object sender, System.EventArgs e)
+        {
+            btnNext.Enabled = checkAgree.Checked ;
+        }
+
+        private void btnNext_Click(object sender, System.EventArgs e)
+        {
+            //var frmLogin = new FrmLogin();
+            //Hide();
+            //frmLogin.ShowDialog();
+            //Close();
+
+        }
+    }
+}
