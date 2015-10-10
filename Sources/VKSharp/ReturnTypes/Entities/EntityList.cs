89cf4efa7397880a901ea29e72e9c12a99f12f4f
kasthack <kasthack@epicm.org>
2015-10-10 08:49:05 +0300

    entitylist is ienumerable

--- Sources/VKSharp/ReturnTypes/Entities/EntityList.cs
+++ Sources/VKSharp/ReturnTypes/Entities/EntityList.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace VKSharp.Core.Entities {
-    public class EntityList<T> /*IEnumerable<T>,*/{
+    public class EntityList<T> : IEnumerable<T> {
         public int Count { get; set; }
         public T[] Items { get; set; }
-        public IEnumerator<T> GetEnumerator() => (IEnumerator<T>) Items.GetEnumerator();
+
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)Items).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ( (IEnumerable) Items ).GetEnumerator();
+
     }
 }
